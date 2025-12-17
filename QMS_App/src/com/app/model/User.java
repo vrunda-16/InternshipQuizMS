0e7da8257bacf098ff9f6547b828def9e5c23e9e
Vrunda <shingarevrunda@gmail.com>
2025-12-17 13:59:41 +0530

    user model

--- QMS_App/src/com/app/model/User.java
+++ QMS_App/src/com/app/model/User.java
@@ -1,5 +1,61 @@
 package com.app.model;
 
 public class User {
+	private int id;
+	private String name;
+	private String email;
+	private String password;
+	private String role;
+	
+	public User() {
+		// TODO Auto-generated constructor stub
+	}
+	
+	public User(int id, String name, String email, String password, String role) {
+		super();
+		this.id = id;
+		this.name = name;
+		this.email = email;
+		this.password = password;
+		this.role = role;
+	}
+	
+	public int getId() {
+		return id;
+	}
 
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	@Override
+	public String toString() {
+		// TODO Auto-generated method stub
+		return "User [id =" + id + ", name = " + name + ", email = " + email + 
+				", password =" + password + ", role = " + "]";
+	}
 }
